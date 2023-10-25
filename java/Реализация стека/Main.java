901d1de8bed18a94d33a859aba3191c23adae181
SashaPavlenko <60470909+SashaPavlenko@users.noreply.github.com>
2023-10-25 03:44:28 +0200

    Update Main.java

--- java/Реализация стека/Main.java
+++ java/Реализация стека/Main.java
@@ -2,10 +2,6 @@ package ru.sfedu.mmcs.stack;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        System.out.println(Tools.parentheses("(a+b)"));
-//        System.out.println(Tools.parentheses("(a+b))"));
-//        System.out.println(Tools.parentheses("((a+b)"));
-//        System.out.println(Tools.parentheses(")("));
         System.out.println(Tools.polRevNot("(a+b)*c"));
         System.out.println(Tools.polRevNot("{c*(a+b)}"));
     }
